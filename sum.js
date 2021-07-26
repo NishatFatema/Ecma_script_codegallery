2cefdcedcb2c099106abad4213cb3b5061aa6de5
NishatFatema <86831426+NishatFatema@users.noreply.github.com>
2021-07-26 23:08:03 +0530

    sum of nums

---
+++ sum.js
@@ -0,0 +1,13 @@
+// const sum=(a,b)=>a+b
+// console.log(sum(2,6))
+
+ // let a=3
+// if (true){
+//     let a=5
+//     console.log(a)
+// }
+// console.log(a)
+
+const name="nishat"
+const message=`welcome ${name}`
+console.log(message)
